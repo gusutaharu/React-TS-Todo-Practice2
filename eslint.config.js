6beac752c755a4f673e2982f4990fc15a9a93074
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2025-12-25 00:50:27 +0900

    create vite eslint v9 の導入

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import pluginReactJSXRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
+import { version } from "react";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    settings:{
+      react: {
+        version: "detect",
+      },
+    },
+    plugins: {
+      js,
+      react: pluginReact,
+    },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+    // rules: {
+    //   "react/react-in-jsx-scope": "off",
+    //   "react/jsx-uses-react": "off",
+    // },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReactJSXRuntime,
+]);
