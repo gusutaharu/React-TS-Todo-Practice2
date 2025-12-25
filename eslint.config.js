e9cf92adad5a17ddd511bee08b7dec8c4d34da43
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2025-12-25 10:33:19 +0900

    prettier,husky,lint-stagedを設定

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,23 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-import js from "@eslint/js";
-import pluginReactJSXRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
-import { version } from "react";
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+import pluginReactJSXRuntime from 'eslint-plugin-react/configs/jsx-runtime.js';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    settings:{
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
     plugins: {
       js,
       react: pluginReact,
     },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
     languageOptions: { globals: globals.browser },
     // rules: {
     //   "react/react-in-jsx-scope": "off",
