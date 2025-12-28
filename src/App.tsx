70d9a56dd17cd22a55dc0e417c57f66f381569e9
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2025-12-28 10:50:17 +0900

    タスク追加機能の実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import { CompleteArea } from './components/CompleteArea';
 import { InCompleteArea } from './components/InCompleteArea';
@@ -5,16 +6,16 @@ import { InputArea } from './components/InputArea';
 import { Todo } from './types/todo';
 
 export const App = () => {
-  const Todos: Todo[] = [
+  const [Todos, setTodos] = useState<Todo[]>([
     { id: 1, text: 'タスク１', isComplete: false },
     { id: 2, text: 'タスク２', isComplete: true },
     { id: 3, text: 'タスク３', isComplete: false },
     { id: 4, text: 'タスク４', isComplete: true },
-  ];
+  ]);
 
   return (
     <div className="w-100 m-2">
-      <InputArea />
+      <InputArea todos={Todos} setTodos={setTodos} />
       <InCompleteArea todos={Todos.filter((todo) => !todo.isComplete)} />
       <CompleteArea todos={Todos.filter((todo) => todo.isComplete)} />
     </div>
--- src/components/InputArea.tsx
+++ src/components/InputArea.tsx
@@ -1,14 +1,49 @@
-export const InputArea = () => {
+import { useForm } from 'react-hook-form';
+import { Todo } from '../types/todo';
+
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+export const InputArea = (props: Props) => {
+  const { todos, setTodos } = props;
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Todo>();
+
+  const onSubmit = (data: Todo) => {
+    const newTodo: Todo = {
+      id: todos.length + 1,
+      text: data.text,
+      isComplete: false,
+    };
+    setTodos([...todos, newTodo]);
+    reset();
+  };
+
   return (
-    <div className="bg-primary rounded-lg p-2 ">
-      <input
-        type="text"
-        placeholder="TODOを入力"
-        className="bg-white rounded-md py-1 px-3"
-      />
-      <button className="bg-gray-200 py-1 px-2 rounded-md ml-1 hover:bg-active hover:text-white hover:cursor-pointer">
-        追加
-      </button>
+    <div>
+      <div className="bg-primary rounded-lg p-2 ">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="text"
+            placeholder="TODOを入力"
+            className="bg-white rounded-md py-1 px-3"
+            {...register('text', { required: '空白です' })}
+          />
+          <button
+            type="submit"
+            className="bg-gray-200 py-1 px-2 rounded-md ml-1 hover:bg-active hover:text-white hover:cursor-pointer"
+          >
+            追加
+          </button>
+        </form>
+      </div>
+      <p className="text-red-600 text-center">{errors.text?.message}</p>
     </div>
   );
 };
